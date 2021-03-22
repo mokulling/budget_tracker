44e169f309f47668fb83d19fa73e7bfc015b7680
mokulling <72229978+mokulling@users.noreply.github.com>
2021-03-21 20:13:42 -0500

    added to webpack config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  entry: "./public/index.js",
+  output: {
+    path: __dirname + "/public/dist",
+    filename: "bundle.js"
+  },
+  mode: "production",
+  plugins: [
+    new WebpackPwaManifest({
+      filename: "manifest.json",
+      inject: false,
+      fingerprints: false,
+      name: "Budget Tracker App",
+      short_name: "Budget app",
+      theme_color: "#ffffff",
+      background_color: "#ffffff",
+      start_url: "/",
+      display: "standalone",
+
+      icons: [
+        {
+          src: path.resolve(
+            __dirname,
+            "public/assets/icons/icon-512x512.png"
+            ),
+          size: [192, 512]
+        }
+      ]
+    })
+  ]
+};
+
+module.exports = config;
